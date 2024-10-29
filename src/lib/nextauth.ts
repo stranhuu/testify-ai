43aefedf7036a056184627a059c09827f850fd29
Purple Grape <sala.tranhuu@gmail.com>
2024-10-28 22:45:05 -0500

    Set up NextAuth.js with prisma

--- src/lib/nextauth.ts
+++ src/lib/nextauth.ts
@@ -1,4 +1,14 @@
 import { NextAuthOptions } from "next-auth";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
 
 export const authOptions: NextAuthOptions = {
+    session: {
+        strategy: 'jwt'
+
+    },
+    callbacks: {
+
+    }
+    secret: process.env.NEXTAUTH_SECRET,
+    adapter: PrismaAdapter(prisma),
 }
